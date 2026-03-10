81e249a301d5cb09bb588c3d4958d4ac9df2c8f7
unknown <triztancanlas0110@gmail.com>
2026-03-11 01:49:06 +0800

    I started speedrunning this by using the map and split function suggested by google

---
+++ batch_1/program_2.py
@@ -0,0 +1,7 @@
+#check if equal
+print("Check if they are equal")
+num1, num2 = map(int, input("Put two numbers: ").split())
+if num1 == num2:
+    print(f"{num1} and {num2} is equal")
+else:
+    print("They are not equal")
