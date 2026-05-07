611f693362bb493ed693c9701b762768fd197708
unknown <triztancanlas0110@gmail.com>
2026-05-07 17:40:40 +0800

    I changed some to make it shorter and organized

--- batch_1/program_10.py
+++ batch_1/program_10.py
@@ -1,3 +1,4 @@
+#print numbers except ending with 0
 for num in range(101):
     if num % 10 == 0:
         continue
--- batch_1/program_8.py
+++ batch_1/program_8.py
@@ -1,12 +1,5 @@
-odd_numbers = []
-
-for i in range(10):
-    try:
-        num = int(input(f"Enter number {i+1} of 10: ")) # Get number input and convert to integer
-        if num % 2 != 0:
-            odd_numbers.append(num)
-    except ValueError:
-        print("Invalid input. Please enter a valid integer.")
-        i -= 1
-
-print("Odd numbers found:", ", ".join(map(str, odd_numbers)))
+count = 0
+for _ in range(10):
+    if int(input("Enter a number: ")) % 2 != 0:
+        count += 1
+print("Odd numbers count:", count) ", ".join(map(str, odd_numbers)))
