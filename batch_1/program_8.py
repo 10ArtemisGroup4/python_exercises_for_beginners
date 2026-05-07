27dce20a5b0739bcf17ec026bc5cb18c39592b1b
unknown <triztancanlas0110@gmail.com>
2026-05-07 17:48:49 +0800

    I saw some pagkakamali and i changed it again

--- batch_1/program_8.py
+++ batch_1/program_8.py
@@ -2,4 +2,4 @@ count = 0
 for _ in range(10):
     if int(input("Enter a number: ")) % 2 != 0:
         count += 1
-print("Odd numbers count:", count) ", ".join(map(str, odd_numbers)))
+print("Odd numbers count:", count)
