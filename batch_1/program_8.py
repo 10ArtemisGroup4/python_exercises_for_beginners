89f65c2c71c3ba5b2b829499bcbc9d048755e0ac
unknown <triztancanlas0110@gmail.com>
2026-05-08 13:08:28 +0800

    Add this

--- batch_1/program_8.py
+++ batch_1/program_8.py
@@ -1,5 +1,5 @@
 count = 0
-for _ in range(10):
-    if int(input("Enter a number: ")) % 2 != 0:
+for i in range(10):
+    if int(input(f"Enter a number {i+1}: ")) % 2 != 0:
         count += 1
 print("Odd numbers count:", count)
